2e3e86c2eaaa33f215bf53a8aa8561082a8baf4a
SantiagoRomeroPineda <saropi9978@gmail.com>
2020-09-14 19:54:31 -0500

    Update cramer.py

--- Tareas_Individuales/Tarea1/cramer.py
+++ Tareas_Individuales/Tarea1/cramer.py
@@ -10,7 +10,7 @@ n=int (input("digite el tamaño de la matriz: "))
 def generarArreglos(n): 
     matriz= ([[0 for j in range(n)] for i in range(n)])
     res= ([0 for i in range(n)])
-    return matriz,res
+    return np.array(matriz), np.array(res)
 
 
 
@@ -37,23 +37,23 @@ def sol_cramer(A,B,R=[]):
 
 
 #ejercicio1
-# matriz=([[1,1],[0.0001,1]])
-# res= ([2,1])
+# matriz=np.array(([[1,1],[0.0001,1]]))
+# res= np.array(([2,1]))
 #n=2
 
 
 #ejercicio3
-#matriz=([[4,-1,-1],[-1,4,-1],[-1,-1,4]])
-#res= ([1,2,3])
-#n=3
+matriz=np.array(([[4,-1,-1],[-1,4,-1],[-1,-1,4]]))
+res= np.array(([1,2,3]))
+n=3
 
 
 #ejercicio4
-# matriz=([[2.6,0.3,2.4,6.2],[7.7,0.4,4.7,1.4], [5.1,9.9,9.5,1.5],[6.0,7.0,8.5,4.8]])
-# res= ([50.78,47.36,91.48,98.17])
+# matriz=np.array(([[2.6,0.3,2.4,6.2],[7.7,0.4,4.7,1.4], [5.1,9.9,9.5,1.5],[6.0,7.0,8.5,4.8]]))
+# res= np.array(([50.78,47.36,91.48,98.17]))
 #n=4
-matriz,res = generarArreglos(n)
-generarMatrices()
+#matriz,res = generarArreglos(n)
+#generarMatrices()
 
 
 print(matriz)
