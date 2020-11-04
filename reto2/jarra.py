cbec158342cee0fbb9ea47478b0633a5dc342490
SantiagoRomeroPineda <saropi9978@gmail.com>
2020-11-04 12:28:18 -0500

    Update jarra.py

--- reto2/jarra.py
+++ reto2/jarra.py
@@ -13,43 +13,40 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 h = -2 #Altura (z)
-b = 1 #Radio 1
+b = 0.76 #Radio 1
 cg = 0
 c = 0.55191502449 #constante para mostrar un circulo
 
 
 
 #base
-for i in range(0, 5):
-
-	p = Circle((0, 0), b, color="green", fill=True)
-
+for i in range(0, 4):
+	p = Circle((0, 0), b, color="green", fill=False)
 	ax.add_patch(p)
-	art3d.pathpatch_2d_to_3d(p, z = h+1.9)
+	art3d.pathpatch_2d_to_3d(p, z = h+2)
 	h = h + 0.1
 
 
 h = 0 #Altura (z)
 b = 0.0 #Radio 2
 #esfera
-for i in np.arange(-1.9,1.9,0.1):
-    p = Circle((0, 0), b, color="green", fill=True)
+for i in np.arange(-1,1.4,0.1):
+    p = Circle((0, 0), b, color="green", fill=False)
     ax.add_patch(p)
-    art3d.pathpatch_2d_to_3d(p, z = i+1.9)
-    b=4-(i*i)
+    art3d.pathpatch_2d_to_3d(p, z = i+1.1)
+    b=2-(i*i)
     print(b)
 
 #paraboloide
 perimetro=0.0
-for i in np.arange(0,4,0.05):
-    perimetro+=0.01
-    p = Circle((0, 0), perimetro, color="green", fill=True)
+for i in np.arange(0,1.2,0.1):
+    p = Circle((0, 0), perimetro, color="green", fill=False)
     ax.add_patch(p)
-    art3d.pathpatch_2d_to_3d(p, z = i)
-
+    perimetro=math.sqrt(i)*3-i    
+    art3d.pathpatch_2d_to_3d(p, z = i+1.5)
     
 
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
-ax.set_zlim(0, 8)
+ax.set_zlim(-1, 5)
 plt.show()
