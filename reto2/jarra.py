fcc906581bd1104ea91d2c091e30347ba1297ed9
SantiagoRomeroPineda <saropi9978@gmail.com>
2020-11-06 09:35:09 -0500

    Update jarra.py

--- reto2/jarra.py
+++ reto2/jarra.py
@@ -2,10 +2,8 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 import numpy as np
 import math
-from mpl_toolkits import mplot3d
-import bezier as Bezier
+from Bezier import Bezier
 from numpy import array as a
-from mpl_toolkits.mplot3d import Axes3D
 import mpl_toolkits.mplot3d.art3d as art3d
 
 def areaParaboloide():
@@ -23,7 +21,9 @@ def areaEsfera():
     return 4* r**2 * math.pi
 
 
-
+def areaPico(l=1):
+  area = (l*l/2)*3
+  return area
 
 def volumenEsfera():
     r=math.sqrt(2)
@@ -64,21 +64,53 @@ def esfera(curvaNivel=False):
         ax.add_patch(p)
         art3d.pathpatch_2d_to_3d(p, z = i+1.1)
         b=2-(i*i)
+        print(b)
 
 def paraboloide(curvaNivel=False):
     #paraboloide
-    radio=0.0
-    for i in np.arange(0,1.2,0.1):
+    radio=0.79
+    for i in np.arange(1,1.7,0.1):
         p = Circle((0, 0), radio, color="green", fill=curvaNivel)
         ax.add_patch(p)
-        radio=math.sqrt(i)*3-i    
+        if i<0.5:
+            radio-=0.1
+        else:
+            radio+=0.1    
         art3d.pathpatch_2d_to_3d(p, z = i+1.5)
-        print(radio)
+        
+#pico
+def pico(curvaNivel=False):
+    aumento=0.1
+    rango= 6
+    aumentoX=0.4
+    h=2.6
+    p=1
+    if curvaNivel:
+        aumento=0.025
+        rango=18
+        aumentoX=0.1
+    
+     
+    b=0.6
+    x=0
+    for i in range(0, rango):
+        points_set_1 = a([[-b, -1, h], [-1, -p - x, h], [1, -p - x, h], [b, -1, h]])
+        t_points = np.arange(0, 1, 0.01)
+        curve_set_1 = Bezier.Curve(t_points, points_set_1)
+        ax.plot(curve_set_1[:, 0], curve_set_1[:, 1], curve_set_1[:, 2], color="green")
 
-cilindro(True)
-esfera(True)
-paraboloide(True)
-area=areaCilindro()+areaEsfera()+areaParaboloide()-(abs(areaCilindro()-areaEsfera()))-(abs(areaParaboloide()-areaEsfera()))
+        h = h + aumento
+        x = x + aumentoX
+# cilindro()
+# esfera()
+# paraboloide()
+pico()
+
+
+
+
+
+area=areaPico()+areaCilindro()+areaEsfera()+areaParaboloide()-(abs(areaCilindro()-areaEsfera()))-(abs(areaParaboloide()-areaEsfera()))
 volumen= volumenCilindro()+volumenEsfera()+volumenParaboloide()-(abs(volumenCilindro()-volumenEsfera()))-(abs(volumenParaboloide()-volumenEsfera()))
 print("el área de la jarra es: ",area)
 print("el volumen de la jarra es: ",volumen)
@@ -88,4 +120,7 @@ print("el volumen de la jarra es: ",volumen)
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 ax.set_zlim(-1, 5)
+ax.set_xlabel('eje x')
+ax.set_ylabel('eje y')
+ax.set_zlabel('eje z')
 plt.show()
