287a0d9a4120f0506e390c27993f0a4c4a73f0d3
SantiagoRomeroPineda <saropi9978@gmail.com>
2020-11-25 09:27:57 -0500

    Update simuladorSIERV.R

--- Reto_3/simuladorSIERV.R
+++ Reto_3/simuladorSIERV.R
@@ -29,28 +29,28 @@ ui <- fluidPage(
                   value = 10),
       sliderInput("r",
                   "Valor del radio:",
-                  min = 0.1,
-                  max = 4,
+                  min = 0.5,
+                  max = 2,
                   value = 0.5),
       sliderInput("beta",
                   "Valor beta (tasa de infeccion):",
                   min = 0.0001,
-                  max = 0.0009,
+                  max = 0.0006,
                   value = 0.0003),
       sliderInput("gamma",
                   "Valor gamma (tasa recuperacion de nodos infectados):",
-                  min = 0.0010,
-                  max = 0.0090,
+                  min = 0.001,
+                  max = 0.005,
                   value = 0.0025),
       sliderInput("alpha",
                   "Valor alpha (tasa de nodos a suceptible):",
-                  min = 0.1, 
-                  max = 0.9,
-                  value = 0.4),
+                  min = 0.041,
+                  max = 0.300,
+                  value = 0.100),
       sliderInput("sigma",
                   "Valor sigma (tasa de vacunación):",
                   min =  0.001,
-                  max =  0.009,
+                  max =  0.003,
                   value =  0.0014),
       sliderInput("S",
                   "Valor de total de suceptibles: ",
@@ -60,34 +60,33 @@ ui <- fluidPage(
       sliderInput("I",
                   "Valor de total de infectados: ",
                   min = 1,
-                  max = 50,
+                  max = 9,
                   value = 5),
       sliderInput("R",
                   "Valor de total de recuperados: ",
                   min = 0,
-                  max = 50,
+                  max = 5,
                   value = 0),
       sliderInput("V",
                   "Valor de vacunados: ",
                   min = 0,
-                  max = 50,
+                  max = 5,
                   value = 0),
       sliderInput("E",
                   "Valor de Expuestos : ",
                   min = 1,
-                  max = 50,
+                  max = 9,
                   value = 5),
       sliderInput("Tiempo",
                   "Dias",
                   min = 100,
-                  max = 1200,
-                  value = 1000)
+                  max = 1000,
+                  value = 500)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("distPlot"),
-      #fluidRow(column(2,tableOutput("table1")))
       tableOutput("table1")
     )
   )
@@ -102,16 +101,16 @@ server <- function(input, output) {
     #Se limpia la consola para una mejor visualizacion
     library(deSolve)
     
-    #Modelo basado en "Modeling and Analysis of Worm Propagation in Wireless Sensor Networks"
+    #Modelo basado en el escrito "An Epidemic model of Malware"
     
     SIR <- function(t, x, parametros){
       with(as.list(c(parametros, x)),{
         N <- sum(v_iniciales)
         dS <- (miu*N) - (phi*S*I) - (miu*S) -(siggma*S) + (epsilon*V) + (delta*R)
-        dE <- (phi*S*I) - ((miu+alpha)*E)
-        dI <- (alpha*E) - ((miu+gammaa)*I)
-        dR <- (gammaa*I) - ((miu+delta)*R)
-        dV <- (siggma*S) - ((miu+epsilon)*V)
+        dE <- (phi*S*I) - (miu+alpha)*E
+        dI <- (alpha*E) - (miu+gammaa)*I
+        dR <- (gammaa*I) - (miu+delta)*R
+        dV <- (siggma*S) - (miu+epsilon)*V
         
         return(list(c(dS, dI, dR, dV, dE)))
       })
@@ -125,13 +124,15 @@ server <- function(input, output) {
       epsilon=0.001,
       siggma = input$sigma,
       delta= 0.001,
-      phi=(input$beta*(input$r*input$r)*3.1416)/(input$L*input$L))
+      phi=(input$beta*input$r*input$r*3.14)/(input$L*input$L))
     
     v_iniciales <- c(S=input$S, I=input$I, R=input$R, V=input$V, E=input$E)
     
     dt <- seq(100, input$Tiempo, 10)
     
     sol = ode(y=v_iniciales, times=dt, func=SIR,parms=parametros, method = input$tipoModelo)
+    
+    # result = EpiDynamics::SIR(pars = parametros, init = v_iniciales, time = dt)
     
     simulacion.si <- as.data.frame(sol)
     attach(simulacion.si)
@@ -148,8 +149,6 @@ server <- function(input, output) {
            col=c("blue", "red", "green","brown", "orange"), lty=rep(1, 2))
     
     
-    # #Calcular R0
-    
     r0r <- (((input$beta)*(3.1416)*((input$r)*(input$r))*(sum(v_iniciales))*(0.001 + 0.001)*(input$alpha))/((input$L * input$L)*(0.001 + input$alpha)*(0.001 + input$gamma)*(0.001 + 0.001 + input$sigma)))
     
     p0 <- (sum(v_iniciales)/((input$L)*(input$L)))*10
@@ -159,9 +158,7 @@ server <- function(input, output) {
     
     output$table1 <- renderTable(data.frame("Variable" = c("Poblacion N=S+E+I+R+V","Densidad de nodos p","R0 segun el radio de comunicacion r","R0 segun la densidad de nodos p","Periodo Infeccioso 1/gamma (Dias)","Periodo Latente 1/sigma (Dias)","Alcance de un nodo suceptible o expuesto (Nodos)"),
                                             "Valor" = c(sum(c(S=input$S, I=input$I, R=input$R, V=input$V, E=input$E)),p0,r0r,r0p,(1/input$gamma),(1/input$sigma),((input$S*(3.1416)*(input$r*input$r))/input$L)  )
-    )
-    )
-    
+    ))
   })
 }
 
